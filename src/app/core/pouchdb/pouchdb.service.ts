7be771f3233ac80592700913089dedee0075f75b
rodridiaz <rodridm@gmail.com>
2019-08-14 20:21:32 +0200

    simplify async callback functions using async/await on pouchdb service

--- src/app/core/pouchdb/pouchdb.service.ts
+++ src/app/core/pouchdb/pouchdb.service.ts
@@ -14,35 +14,40 @@ export class PouchdbService {
       this.db = new PouchDB('exercisesDb');
       this.isInstantiated = true;
     }
+    //
+    // Use the code below to replicate the dataBase on a CouchDB or PouchDB Server
+    //
+    // PouchDB.replicate('exercisesDb', 'http://localhost:5984/exercisesDb', {
+    //   live: true
+    // });
   }
 
-  public fetch() {
+  public async fetch() {
     return this.db.allDocs({ include_docs: true });
   }
 
-  public get(id: string): Promise<any> {
+  public async get(id: string): Promise<any> {
     return this.db.get(id).then(result => result, error => error);
   }
 
-  public put(id: string, document: any): Promise<any> {
+  public async put(id: string, document: any): Promise<any> {
     let doc = null;
-    return this.get(id).then(
-      result => {
-        doc = result;
-        if (doc) {
-          return this.db.put({
-            ...document,
-            _id: id,
-            _rev: doc._rev,
-            createdDate: new Date()
-          });
-        }
-        return this.db.put({ ...document, _id: id, createdDate: new Date() });
-      },
-      error => {
-        if (error.status !== 404) throw new Error(error);
-      }
-    );
+
+    try {
+      doc = await this.get(id);
+    } catch (e) {
+      if (e.status !== 404) throw new Error(e);
+    }
+
+    if (doc) {
+      return this.db.put({
+        ...document,
+        _id: id,
+        _rev: doc._rev,
+        createdDate: new Date()
+      });
+    }
+    return this.db.put({ ...document, _id: id, createdDate: new Date() });
   }
 
   public async remove(id: string): Promise<any> {
